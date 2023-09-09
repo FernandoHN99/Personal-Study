60dbc5df50aef634e83e4bec099613b766ce6e4c
Fernando Henriques Neto <nandohneto@gmail.com>
2023-09-09 04:55:39 -0300

    ajuste fino busca

---
+++ 1-GuiasHelper/JavaScript/JS-Resumo/MetodosArray/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+
+// var maior_nota = Math.max(...estudantes.map(i=> i.nota))
+// console.log(maior_nota)
+
+// var notas_normalizadas = estudantes.map(
+//     (estudante) => (estudante.nota*10/maior_nota).toFixed(2) 
+// )
+// console.log(notas_normalizadas)
+    
+    
