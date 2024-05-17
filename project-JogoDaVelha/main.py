c8f589642867d72477cee17b8b313e4d96cd9cd1
FernandoHN99 <nandohneto@gmail.com>
2024-05-17 14:18:17 -0300

    Implementa selecao de opcao do jogador

--- project-JogoDaVelha/main.py
+++ project-JogoDaVelha/main.py
@@ -1,11 +1,36 @@
+import string 
+import os
+
+''' Util '''
+
+def limpar_tela():
+   os.system('clear')
+
 def criar_matriz_quadrada(n_linhas):
 	return [[' ' for _ in range(n_linhas)] for _ in range(n_linhas)]
 
-def gerar_lista_letras(n):
-    return [chr(i) for i in range(ord('A'), ord('A') + n)]
+def criar_dicionario_letra_numero(n_colunas):
+    dicionario = {}
+    lista_letras = [chr(i) for i in range(ord('A'), ord('A') + n_colunas)]
+    for i, letra in enumerate(string.ascii_uppercase[:len(lista_letras)]):
+        dicionario[letra] = i
+    return dicionario
 
-def imprimir_matriz(mat):
-    print("😡 = Computador     😎 = Humano\n")
+def contar_jogadas_restantes():
+    return sum(1 for linha in mat for elem in linha if elem == " ")
+   
+def validar_coluna(coluna):
+      return coluna in dic_colunas.keys()
+
+def validar_linha(linha):
+      return 0 <= linha < len(mat)
+
+def validar_jogada(linha, coluna):
+   return validar_coluna(coluna) and validar_linha(linha) and mat[linha][dic_colunas[coluna]] == " "
+      
+
+def imprimir_jogo():
+    print("O = Computador     X = Humano\n")
     for i in range(1, len(mat)+1):
         print(f"{i:2}", end=" ")
         for j in range(1, len(mat[0])+1):
@@ -16,15 +41,36 @@ def imprimir_matriz(mat):
                 print(f"  {jogada}  ", end="")
         print(f"\n   {(' - - -' * len(mat[0]))[:-1]}")
     print("     ", end="")
-    lista_letras = gerar_lista_letras(len(mat[0]))
-    for letra in lista_letras:
+    for letra in dic_colunas.keys():
         print(f"{letra:4}  ", end="")
-    print()
+    print("\n")
 
 
-mat = criar_matriz_quadrada(7)
-mat[0][0] = 'X'
-mat[0][2] = 'X'
-mat[1][1] = 'O'
-mat[0][6] = 'X'
-print(imprimir_matriz(mat))
+def perguntar_jogada():
+   eh_jogada_valida = False
+   while not eh_jogada_valida:
+      limpar_tela()
+      imprimir_jogo()
+      try:
+         coluna = input(f"Digite a coluna: ").upper()
+         linha = int(input(f"Digite a linha: ")) - 1 
+         eh_jogada_valida = validar_jogada(linha, coluna)
+         if not eh_jogada_valida:
+            input("Jogada inválida! Enter para continuar.")
+      except ValueError:
+         input("Jogada inválida! Enter para continuar.")
+         continue
+   return linha, dic_colunas[coluna]
+''' main '''
+n_colunas = 5
+mat = criar_matriz_quadrada(n_colunas)
+dic_colunas = criar_dicionario_letra_numero(n_colunas)
+def main():
+   while True:
+      linha, coluna = perguntar_jogada()
+      mat[linha][coluna] = "X"
+      limpar_tela()
+      imprimir_jogo()
+      break
+
+main()
