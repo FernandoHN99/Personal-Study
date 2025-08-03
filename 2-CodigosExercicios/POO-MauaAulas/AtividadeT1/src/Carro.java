b4f876da7e23fe714fc3aa2b8dda4d30cde3ba7a
FernandoHN99 <nandohneto@gmail.com>
2025-08-03 18:33:25 -0300

    Ajustando Java

--- 2-CodigosExercicios/POO-MauaAulas/AtividadeT1/src/Carro.java
+++
@@ -1,8 +0,0 @@
-public class Carro extends Transporte {
-
-    public Carro(){
-        this.idTransporte = gerarID();
-        this.custoPorHora = 59.99;
-        this.disponibilidade = true;
-    }
-}
--- POO-MauaAulas/T1-Atividade/src/Patinete.java
+++
@@ -1,9 +0,0 @@
-public class Patinete extends Transporte {
-
-    public Patinete(){
-        this.idTransporte = gerarID();
-        this.custoPorHora = 19.99;
-        this.disponibilidade = true;
-    }
-    
-}
