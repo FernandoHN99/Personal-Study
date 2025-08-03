688944aa71063acbcf48125a295f3b02bd70c707
FernandoHN99 <nandohneto@gmail.com>
2025-08-03 18:32:26 -0300

    Ajustando Java

---
+++ 01-POOAulas/T1-Atividade/src/Moto.java
@@ -0,0 +1,9 @@
+public class Moto extends Transporte {
+
+    public Moto(){
+        this.idTransporte = gerarID();
+        this.custoPorHora = 39.99;
+        this.disponibilidade = true;
+    }
+    
+}
---
+++ 01-POOAulas/T1-Atividade/src/Patinete.java
@@ -0,0 +1,9 @@
+public class Patinete extends Transporte {
+
+    public Patinete(){
+        this.idTransporte = gerarID();
+        this.custoPorHora = 19.99;
+        this.disponibilidade = true;
+    }
+    
+}
