1eca929436ab135a57e53d69f6a5db2156e0b521
FernandoHN99 <nandohneto@gmail.com>
2025-08-03 16:53:20 -0300

    Ajustando HTML-CSS

--- 1-GuiasHelper/JavaScript/JS-Resumo/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function getUser() {
-    return new Promise((resolve, reject) => { //Aqui a promisse é criada com status pending
-      // console.log("DEU CERTO"); //Síncrono
-      const user = { name: "John", age: 30 };
-      resolve(user);
-    });
-  }
-
-getUser().then(res => console.log(res))
